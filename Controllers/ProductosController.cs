ae4a60759d9de3b87112970054e125ed867643b2
LucaPalminteri <lucapalminteri02@gmail.com>
2022-12-30 19:34:36 -0300

    add productos controller & add 23k data rows in db

---
+++ Controllers/ProductosController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NBase.entities;
+using NBase;
+
+namespace EnFr2.Controllers
+{
+    [ApiController]
+    [Route("api/productos")]
+    public class ProductosController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public ProductosController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Producto>>> GetProductos()
+        {
+            return await context.Productos.ToListAsync();
+        }
+
+
+    }
+}
