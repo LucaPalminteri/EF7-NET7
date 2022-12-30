6242a5f6b9bc6be115fd7987e9b4f773bbc58485
LucaPalminteri <lucapalminteri02@gmail.com>
2022-12-30 18:59:18 -0300

    first commit

---
+++ Controllers/PublicacionesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NBase;
+using NBase.entities;
+
+namespace EnFr2.Controllers
+{
+    [ApiController]
+    [Route("api/publicaciones")]
+    public class PublicacionesController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public PublicacionesController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Publicacion>>> GetPublicaciones()
+        {
+            return await context.Publicaciones.ToListAsync(); 
+        }
+    }
+}
